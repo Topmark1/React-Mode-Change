d3918a5ede3630e49f83eddce1be1d03581f5961
Topmark1 <topsiology11@gmail.com>
2022-11-04 20:08:45 +0100

    contact page completion and styling

--- src/App.js
+++ src/App.js
@@ -4,16 +4,13 @@ import React from 'react'
 import {Linktree} from './linktree/linktree.js'
 import {Contact} from './linktree/contact.js'
 import {Route, Routes} from 'react-router-dom'
-
 //import ModeChange from './modeChange/modeChange.js'// <ModeChange/>
-
 
 export default function App() {
     return (
     <>
     <Routes>
-    <Route  path='/'>
-    
+    <Route  path='/'>    
     <Route path="/Contact" element={<Linktree/>} />
     <Route index element={<Contact/>} />
     </Route>
